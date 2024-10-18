a7f2e7d1a4492068db6b3d4577f0ed29094691c0
faikhadutoglu <167749575+faikhadutoglu@users.noreply.github.com>
2024-10-18 09:50:39 +0200

    Voorlesung

--- exercise-001/main.cpp
+++ exercise-001/main.cpp
@@ -10,15 +10,14 @@ auto main(int argc, char **argv) -> int
      * CLI11 is a command line parser to add command line options
      * More info at https://github.com/CLIUtils/CLI11#usage
      */
-    CLI::App app{PROJECT_NAME};
+    auto counter{20}; /*set a default of 20*/
+    CLI::App app{PROJECT_NAME}; /*create an instance of an app with the variable name app*/
     try
     {
-        int count = 20;
-        app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));
-        app. add_option("-c,--count", counter, "How many items to insert");
-
-        
-        app.parse(argc, argv);
+        //int count = 20;
+        app.set_version_flag("-V,--version", fmt::format("{} {}", PROJECT_VER, PROJECT_BUILD_DATE));/*Add an command line option -c or --count and fill the variable counter*/
+        app. add_option("-c,--count", counter, "How many items to insert");  //kommandozeile eingabe eigenschafthinzugefügt
+        app.parse(argc, argv); 
     }
     catch (const CLI::ParseError &e)
     {
@@ -31,8 +30,11 @@ auto main(int argc, char **argv) -> int
      * More info at https://fmt.dev/latest/api.html
      */
     fmt::print("Hello, {}!\n", app.get_name());
-
+    fmt::print("Value of argc, {}!\n", argc);
+    fmt::print("Value of argv[0], {}!\n", argv);
+    fmt::print("Value of argv[2], {}!\n", argv);
+    fmt::print("The value of counter {}!\n", counter);
     /* INSERT YOUR CODE HERE */
 
-    return 0; /* exit gracefully*/
+    return 42; /* exit gracefully*/
 }
